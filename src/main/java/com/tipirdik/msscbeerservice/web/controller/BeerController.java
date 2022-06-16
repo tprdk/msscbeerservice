753322cb39fb5e6021a77703a8ba06b3b7f3a2d7
tprdk <tprdk@outlook.com>
2022-06-16 22:56:16 +0300

    web beer controller added.

---
+++ src/main/java/com/tipirdik/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,31 @@
+package com.tipirdik.msscbeerservice.web.controller;
+
+import com.tipirdik.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        // todo service implementation will be added.
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        // todo service implementation will be added.
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        // todo service implementation will be added.
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
