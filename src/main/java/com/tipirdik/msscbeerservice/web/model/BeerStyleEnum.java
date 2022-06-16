a26510070ef80bdd7d0ad4b29cb3c6bc7aef2df0
tprdk <tprdk@outlook.com>
2022-06-16 22:49:28 +0300

    web data model added.

---
+++ src/main/java/com/tipirdik/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.tipirdik.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
