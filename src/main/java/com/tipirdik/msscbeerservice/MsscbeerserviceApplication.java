ba8c021297f026d4ccd769c35449f270f8affc55
tprdk <tprdk@outlook.com>
2022-06-16 22:37:08 +0300

    init

---
+++ src/main/java/com/tipirdik/msscbeerservice/MsscbeerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tipirdik.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscbeerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscbeerserviceApplication.class, args);
+	}
+
+}
